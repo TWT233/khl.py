c67b7d7cd40e088bce65ac4f1497dbe4ad99670a
TWT <twt2333@outlook.com>
2020-12-16 15:47:35 +0800

    feat(utils): User.py: class represents normal user

---
+++ khl/utils/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, data):
+        # TODO: dara handler
+        self.id = data['id']
+        self.nickname = data['nickname']
+        pass
--- khl/utils/__init__.py
+++ khl/utils/__init__.py
@@ -1,1 +1,2 @@
+from .User import User
 from .hardcoded import *
