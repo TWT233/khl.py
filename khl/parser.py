f4a4c031736dee03a6687f68b6b37d940b593975
TWT <twt2333@outlook.com>
2021-08-12 21:07:20 +0800

    fix: handle shlex exception
    Closes #9

--- khl/parser.py
+++ khl/parser.py
@@ -7,7 +7,11 @@ from khl.message import TextMsg
 def parser(msg: TextMsg, prefixes: Iterable) -> Tuple[TextMsg, List[str]]:
     for prefix in prefixes:
         if msg.content.startswith(prefix):
-            raw_cmd = shlex.split(msg.content[len(prefix):])
-            return msg, raw_cmd
+            try:
+                raw_cmd = shlex.split(msg.content[len(prefix):])
+            except ValueError:
+                pass
+            else:
+                return msg, raw_cmd
 
     return msg, []
