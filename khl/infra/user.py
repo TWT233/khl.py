03fa761a372ed36e678e3c383f024275b7ebdc8a
TWT <twt2333@outlook.com>
2021-09-01 19:34:49 +0800

    feat(infra): user: define and constructor

---
+++ khl/infra/user.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from .abc import LazyLoadable
+from .role import Role
+
+
+class User(LazyLoadable):
+    """
+    `Standard Object`
+
+    represent a entity that interact with khl server
+    """
+    id: str
+    username: str
+    nickname: str
+    identify_num: str
+    online: bool
+    bot: bool
+    status: int
+    avatar: str
+    vip_avatar: str
+    mobile_verified: bool
+    roles: List[Role]
+
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', '')
+        self.username = kwargs.get('username', '')
+        self.nickname = kwargs.get('nickname', '')
+        self.identify_num = kwargs.get('identify_num', '')
+        self.online = kwargs.get('online', False)
+        self.bot = kwargs.get('bot', False)
+        self.status = kwargs.get('status', 0)
+        self.avatar = kwargs.get('avatar', '')
+        self.vip_avatar = kwargs.get('vip_avatar', '')
+        self.mobile_verified = kwargs.get('mobile_verified', False)
+        self.roles = kwargs.get('roles', [])
+
+        self._loaded = kwargs.get('_lazy_loaded_', False)
+        self._gate = kwargs.get('_gate_', None)
+
+    async def load(self):
+        pass
