ebfc298259de337e3eaa8f28beacc66a5ffd4dcb
TWT <twt2333@outlook.com>
2021-09-01 21:29:17 +0800

    feat(infra): abc/AsyncRunnable: used to set async env

--- khl/infra/abc.py
+++ khl/infra/abc.py
@@ -1,5 +1,5 @@
 r"""Some Interfaces"""
-
+import asyncio
 from abc import ABC, abstractmethod
 
 from ..gateway import Gateway
@@ -54,3 +54,18 @@ class LazyLoadable(Requestable, ABC):
         :return: bool
         """
         return self._loaded
+
+
+class AsyncRunnable(ABC):
+    """
+    Classes that has async work to run
+    """
+    _loop: asyncio.AbstractEventLoop = None
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return self._loop
+
+    @loop.setter
+    def loop(self, new_loop: asyncio.AbstractEventLoop):
+        self._loop = new_loop
