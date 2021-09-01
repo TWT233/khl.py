1eb253f6b03e7e0d85a96016ffec02141f22aca6
TWT <twt2333@outlook.com>
2021-09-01 20:33:32 +0800

    feat(infra): context: define and constructor

---
+++ khl/infra/context.py
@@ -0,0 +1,13 @@
+from .abc import Requestable
+from .channel import Channel
+from .guild import Guild
+
+
+class Context(Requestable):
+    channel: Channel
+    guild: Guild
+
+    def __init__(self, **kwargs):
+        self.channel = kwargs.get('channel')
+        self.guild = kwargs.get('guild')
+        self._gate = kwargs.get('_gate_')
