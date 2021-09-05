44cdbbec82bf6dac3c0b7d917d4a4f7476444c23
TWT <twt2333@outlook.com>
2021-09-05 16:14:28 +0800

    refactor: take requester and api list apart

---
+++ khl/api_list.py
@@ -0,0 +1,111 @@
+import functools
+import inspect
+import logging
+import re
+from typing import Dict, Callable
+
+log = logging.getLogger(__name__)
+
+
+class _Req:
+    def __init__(self, method: str, route: str, params: Dict):
+        self.method = method
+        self.route = route
+        self.params = params
+
+
+def req(method: str):
+    def _method(func: Callable):
+        @functools.wraps(func)
+        def req_maker(*args, **kwargs) -> _Req:
+            param_names = list(inspect.signature(func).parameters.keys())
+            for i in range(len(args)):
+                kwargs[param_names[i]] = args[i]
+            route = re.sub(r'(?<!^)(?=[A-Z])', '-', func.__qualname__).lower().replace('.', '/')
+            return _Req(method, route, kwargs)
+
+        return req_maker
+
+    return _method
+
+
+class Channel:
+    @staticmethod
+    @req('GET')
+    def list(
+            guild_id
+    ):
+        ...
+
+    @staticmethod
+    @req('GET')
+    def view(
+            target_id
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def create(
+            guild_id,
+            parent_id,
+            name,
+            type,
+            limit_amount,
+            voice_quality,
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def delete(
+            channel_id
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def moveUser(
+            target_id,
+            user_ids
+    ):
+        ...
+
+
+class ChannelRole:
+
+    @staticmethod
+    @req('GET')
+    def index(
+            channel_id
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def create(
+            channel_id,
+            type,
+            value,
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def update(
+            channel_id,
+            type,
+            value,
+            allow,
+            deny,
+    ):
+        ...
+
+    @staticmethod
+    @req('POST')
+    def delete(
+            channel_id,
+            type,
+            value,
+    ):
+        ...
--- khl/requester.py
+++ khl/requester.py
@@ -1,25 +1,16 @@
 import asyncio
-import functools
-import inspect
 import logging
-import re
-from typing import Union, Dict, Callable
+from typing import Union
 
 from aiohttp import ClientSession
 
+from .api_list import _Req
 from .cert import Cert
 from .interface import AsyncRunnable
 
 log = logging.getLogger(__name__)
 
 API = f'https://www.kaiheila.cn/api/v3'
-
-
-class _Req:
-    def __init__(self, method: str, route: str, params: Dict):
-        self.method = method
-        self.route = route
-        self.params = params
 
 
 class HTTPRequester(AsyncRunnable):
@@ -46,100 +37,3 @@ class HTTPRequester(AsyncRunnable):
 
     async def exec_req(self, r: _Req):
         return await self.request(r.method, r.route, **r.params)
-
-
-def req(method: str):
-    def _method(func: Callable):
-        @functools.wraps(func)
-        def req_maker(*args, **kwargs) -> _Req:
-            param_names = list(inspect.signature(func).parameters.keys())
-            for i in range(len(args)):
-                kwargs[param_names[i]] = args[i]
-            route = re.sub(r'(?<!^)(?=[A-Z])', '-', func.__qualname__).lower().replace('.', '/')
-            return _Req(method, route, kwargs)
-
-        return req_maker
-
-    return _method
-
-
-class Channel:
-    @staticmethod
-    @req('GET')
-    def list(
-            guild_id
-    ):
-        ...
-
-    @staticmethod
-    @req('GET')
-    def view(
-            target_id
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def create(
-            guild_id,
-            parent_id,
-            name,
-            type,
-            limit_amount,
-            voice_quality,
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def delete(
-            channel_id
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def moveUser(
-            target_id,
-            user_ids
-    ):
-        ...
-
-
-class ChannelRole:
-
-    @staticmethod
-    @req('GET')
-    def index(
-            channel_id
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def create(
-            channel_id,
-            type,
-            value,
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def update(
-            channel_id,
-            type,
-            value,
-            allow,
-            deny,
-    ):
-        ...
-
-    @staticmethod
-    @req('POST')
-    def delete(
-            channel_id,
-            type,
-            value,
-    ):
-        ...
