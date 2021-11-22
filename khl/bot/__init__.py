ee7d9d7c92fdedc99bcfbb8050e64a2375abdc60
TWT <twt2333@outlook.com>
2021-11-22 22:24:43 +0800

    refactor(rule): re construct

--- khl/bot/__init__.py
+++ khl/bot/__init__.py
@@ -1,5 +1,5 @@
+from . import rule as Rule
 from .bot import Bot
 from .command import Command
 from .lexer import Lexer, DefaultLexer, RELexer
 from .parser import Parser
-from .rule import Rule, CommandRule
