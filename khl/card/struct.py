7c16a095baa92182adb77c190f0d09ae6d2d110c
omoidesu <949395971@qq.com>
2022-01-28 11:41:36 +0800

    feat(card): 补充Module.Container, Struct

---
+++ khl/card/struct.py
@@ -0,0 +1,36 @@
+from .element import Element
+from .interface import _Struct, SizeTypes, ThemeTypes
+
+from typing import Union, List, Dict
+
+from abc import ABC
+
+class Paragraph(_Struct):
+    _type = 'paragraph'
+    _cols: int
+    _fields: List[Element.Text]
+
+    def __init__(self, cols: int, *fields: Element.Text):
+        if not 1<= len(fields) <= 50:
+            raise ValueError('fields length unacceptable, should: 50 >= len >= 1')
+        self._cols = cols
+        self._fields = list(fields)
+        super().__init__(ThemeTypes.NA, SizeTypes.NA)
+
+    def append(self, field: Element.Text):
+        if len(self._fields) >= 50:
+            raise ValueError('fields max length exceeded(50)')
+        self._fields.append(field)
+
+    def pop(self, index: int) -> Element.Text:
+        if len(self._fields) <= 1:
+            raise ValueError('fields min length exceeded(1)')
+        return self._fields.pop(index)
+
+    def len(self):
+        return len(self._fields)
+
+    @property
+    def _repr(self) -> Union[str, Dict]:
+        return self._gen_dict(['type', 'cols', 'fields'])
+
