dc1fc2bf99632c309e77d4e6e8d3243c88f16561
TWT <twt2333@outlook.com>
2022-01-29 00:24:05 +0800

    docs: fit new version khl.py

--- example/ex01_helloworld/ex01.py
+++ example/ex01_helloworld/ex01.py
@@ -7,7 +7,7 @@ bot = Bot(token='xxxxxxxxxxxxxxxxxxxxxxxxx')
 # register command
 # invoke this via saying `/hello` in channel
 @bot.command(name='hello')
-async def world(msg: Message):
+async def world(msg: Message):  # when `name` is not set, the function name will be used
     await msg.reply('world!')
 
 
