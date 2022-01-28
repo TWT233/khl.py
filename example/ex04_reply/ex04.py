b3566d5c4090913c2968390409da31e54ecc43cc
TWT <twt2333@outlook.com>
2022-01-29 00:24:05 +0800

    docs: fit new version khl.py

--- example/ex04_reply/ex04.py
+++ example/ex04_reply/ex04.py
@@ -1,6 +1,6 @@
 import json
 
-from khl import Bot, PublicMessage
+from khl import Bot, Message
 
 # load config from config/config.json, replace `path` to your own config file
 # config template: `./config/config.json.example`
@@ -14,17 +14,17 @@ bot = Bot(token=config['token'])
 # register command
 # invoke this via saying `!hello` in channel
 @bot.command(name='hello')
-async def roll(msg: PublicMessage):
+async def roll(msg: Message):
     # quote reply
     await msg.reply('world for you!')
     # msg from reply_temp & send_temp will be cleaned when you restart client or refresh browser
     await msg.reply('world only for you!', is_temp=True)
 
     # send to current channel
-    await msg.channel.send('world for all!')
+    await msg.ctx.channel.send('world for all!')
     # send to current channel, but only visible to `msg.author`
     # same as `msg.reply_temp()`, but can set `temp_target_id` explicitly
-    await msg.channel.send('world only for you too!', temp_target_id=msg.author.id)
+    await msg.ctx.channel.send('world only for you too!', temp_target_id=msg.author.id)
 
 
 # everything done, go ahead now!
