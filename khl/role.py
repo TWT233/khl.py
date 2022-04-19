7c8d8f795635e6fac1e24633fba78742673f28f6
DancingSnow <1121149616@qq.com>
2022-04-19 12:12:22 +0800

    feature(role): add has_permission method

--- khl/role.py
+++ khl/role.py
@@ -20,3 +20,14 @@ class Role:
         self.hoist: int = kwargs.get("hoist", 0)
         self.mentionable: int = kwargs.get("mentionable", 0)
         self.permissions: int = kwargs.get("permissions", 0)
+
+        self._permissions = self._perm_to_bit()
+
+    def _perm_to_bit(self, count=28) -> str:
+        return "".join([str((self.permissions >> y) & 1) for y in range(count - 1, -1, -1)])
+
+    def has_permission(self, bit_value: int) -> bool:
+        """
+        bitValue see https://developer.kaiheila.cn/doc/http/guild-role#%E6%9D%83%E9%99%90%E8%AF%B4%E6%98%8E
+        """
+        return self._permissions[27 - bit_value] == '1'
