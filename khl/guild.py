e306904e1a1edff80e31627b0493eed0b88d8f4f
Deerio Chaelingo <DeeChael@outlook.com>
2022-12-22 19:24:25 +0800

    feat: guild emoji class

--- khl/guild.py
+++ khl/guild.py
@@ -34,6 +34,31 @@ class GuildBoost:
         self.user = User(**kwargs.get('user'), _gate_=kwargs.get('_gate_', None))
 
 
+class GuildEmoji(Requestable):
+    """
+    The custom emojis in the guild
+    """
+    guild_id: str
+    name: str
+    id: str
+    user: GuildUser
+
+    def __init__(self, **kwargs):
+        self.gate = kwargs.get('_gate_', None)
+        self.guild_id = kwargs.get("guild_id")
+        self.name = kwargs.get("name")
+        self.id = kwargs.get("id")
+        self.user = GuildUser(_gate_=self.gate, guild_id=self.guild_id, **kwargs.get("user_info"))
+
+    async def update(self, name: str):
+        """update this emoji's name"""
+        await self.gate.exec_req(api.GuildEmoji.update(id=self.id, name=name))
+
+    async def delete(self):
+        """delete this custom emoji"""
+        await self.gate.exec_req(api.GuildEmoji.delete(id=self.id))
+
+
 class ChannelCategory(PermissionHolder, Requestable):
     """represent a channel set"""
 
