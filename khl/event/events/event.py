e2e87d4a55a1840e60bf6f15d07bccb1be2f509a
DancingSnow <1121149616@qq.com>
2023-11-13 17:55:33 +0800

    docs(event): add docstring for AbstractEvent

--- khl/event/events/event.py
+++ khl/event/events/event.py
@@ -2,9 +2,12 @@ from abc import ABC
 
 
 class AbstractEvent(ABC):
+    """base event"""
+
     def __init__(self, raw: dict):
         self._raw = raw
 
     @property
     def raw(self):
+        """return the event raw body"""
         return self._raw
